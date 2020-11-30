5fd8dff7f4012b0a5c8352b2a6639a7249fcb0f2
dianajesse <59080334+dianajesse@users.noreply.github.com>
2020-11-29 17:03:31 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Login from './Login';
 import SignUp from './SignUp';
 import Home from './Home';
-import Footer from './Footer';
 import NavBar from './OriNavbar/NavBar';
+import Upload from './Upload';
+
 import {BrowserRouter as Router,Switch,Route} from  'react-router-dom';
 
 
 function App() {
   return (
   <Router>
-    <NavBar/>
     <Switch>
-    <Route exact path='/' component={Footer}/>
-    <Route exact path='/' component={Home}/>
-    <Route exact path='/Home' component={Home}/>
+    <Route path='/Upload' component={Upload}/>
     <Route path='/login' component={Login}/>
     <Route path='/SignUp' component={SignUp}/>
+    <NavBar/>
+    <Route exact path='/' component={Home}/>
+    <Route exact path='/Home' component={Home}/>
+    
     </Switch>
   </Router>  
   )
--- Login.js
+++ Login.js
@@ -1,38 +1,75 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
+import {Button} from 'react-bootstrap'
+import {BrowserRouter as Router,Switch,Route} from  'react-router-dom';
+import About from './About';
+
 
 function Login() {
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const [LoggedIn,setLoggedIn]=useState(false)
+
+    const handleEmailInput=(e)=>{setEmail(e.target.value)}
+    const handlePasswordInput=(e)=>{setPassword(e.target.value)}
+    
+    function handleLogin (e){
+        e.preventDefault()
+    if(email==="diana@gmail"&& password==="1234"){
+           setLoggedIn(true);
+           <Router><Switch> 
+           <Route exact path='/About' component={About}/>
+             </Switch></Router>
+    }
+    
+      }
+      function handleLogout (e){
+          e.preventDefault();
+          setLoggedIn(false)
+        }
     return (
-   <div className="Login-component">
+        <div>{
+
+            LoggedIn
+           
+            ?
+            <>
+           <About/>
+           <Button style={{align:"right"}} onClick={handleLogout}> LogOut</Button> 
+            </>
+              :
+            <> 
+            <div className="Login-component">
                 <div >
-                    {<h1 className="Welcome" style={{color:"white"}}>Welcome User</h1>}
+                    {<h1 className="Welcome" style={{color:"white"}}>Welcome User </h1>}
                 </div>
+           
+           
     <div className="centered">
         <div className="box">
             <form>
-                <h3 style={{textAlign:"center"}}><i>LoGin PaGe</i></h3>
+                <h3 style={{textAlign:"center"}}><i><span>LoGin PaGe</span></i></h3>
                 <p>
-                <label for="fname"><b>UserName:</b></label>
-                <input type="text" id="fname"  name="fname" placeholder="Diana"/><br></br>
+                <label for="lname" className="distance"><b>E-mail:</b></label>
+                        <input type="text" value={email}  onChange={handleEmailInput} placeholder="diana@gmail"/><br></br>
                 </p>
                 <p>
-                <label for="lname"><b>E-mail:</b></label>
-                <input type="text" id="lname" name="lname" placeholder="diana@gmail"/><br></br>
-                </p>
-                <p>
-                <label for="lname"><b>PassWord:</b></label>
-                <input type="text" id="lname" name="lname"  placeholder="12@hk/?"/><br></br>
+                <label for="lname"className="distance"><b>PassWord:</b></label>
+                <input type="text" value={password} onChange={(e)=>handlePasswordInput(e)}  placeholder="12@hk/?"/><br></br>
                 </p> 
                  <p style={{textAlign:"center"}}>
-                <button><input type="button" value="Submit"/></button>
-                <Link to ='/SignUp'><button><input type="button" value="SignUp"/></button></Link>
+                 <input type="button" value="Submit" onClick={handleLogin}/>
+                 <Link to='/SignUp'><button>SignUp</button></Link>
                 </p> 
             </form>
         </div>
         </div>
     </div>
-                  
-    )
-}
+    </>
+      }     
+       </div>
+       )
+        }
+   
 
 export default Login
